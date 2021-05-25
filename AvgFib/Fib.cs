aa4b7bfa1ef18981afd83727627a60cca37d77af
Greg Doud <gpdoud@gmail.com>
2021-05-25 09:30:35 -0400

    Initial commit

---
+++ AvgFib/Fib.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+namespace Doud.Fib {
+    public class Fib {
+
+        public static List<int> GenerateSequence(int last = 200) {
+            var fibs = new List<int>();
+            var a = 0;
+            fibs.Add(a);
+            var b = 1;
+            fibs.Add(b);
+            int c = 0;
+            while(c < last) {
+                c = a + b;
+                fibs.Add(c);
+                a = b;
+                b = c;
+            }
+            return fibs;
+        }
+    }
+}
--- AvgFib/Program.cs
+++ AvgFib/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Doud.Fib;
 /*
  * You are to calculate the average of the sequence of the fibinacci sequence that ends with the first
  * number greater than 200. Store the generated numbers in a List<int> collection then calculate the
@@ -7,6 +9,13 @@
 namespace AvgFib {
     class Program {
         static void Main(string[] args) {
+            List<int> fibs = Fib.GenerateSequence(200);
+            var sum = 0;
+            foreach(var fib in fibs) {
+                sum = sum + fib;
+            }
+            var avg = sum / fibs.Count;
+            Console.WriteLine($"Fibs avg is {avg}");
         }
     }
 }
