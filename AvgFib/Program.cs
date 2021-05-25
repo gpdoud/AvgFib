d36e5409677791ead17d153c3b284bd5c7af8ed6
Greg Doud <gpdoud@gmail.com>
2021-05-25 09:17:10 -0400

    Add project files.

---
+++ AvgFib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/*
+ * You are to calculate the average of the sequence of the fibinacci sequence that ends with the first
+ * number greater than 200. Store the generated numbers in a List<int> collection then calculate the
+ * average and display it using a Console.WriteLine statement
+ */
+namespace AvgFib {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
